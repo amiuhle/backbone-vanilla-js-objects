75e40081fc8e4a1d0f6fb2514c7525f9115ce70d
Timo Uhlmann <timouhlmann@googlemail.com>
2013-04-23 21:14:45 +0200

    Created necessary MVC and write some specs for it

--- backbone-vanilla-js-objects.js
+++ backbone-vanilla-js-objects.js
@@ -1,8 +1,35 @@
 (function () {
   Backbone.VanillaJsObjects = {};
+
+  var isArray = function(obj) {
+    return Object.prototype.toString.apply(obj) === '[object Array]'
+  };
+
+  Backbone.VanillaJsObjects.Object = Backbone.Collection.extend({
+
+  });
+
+  Backbone.VanillaJsObjects.Array = Backbone.Collection.extend({
+
+  });
+  
+  Backbone.VanillaJsObjects.Property = Backbone.Model.extend({
+
+  });
+
   Backbone.VanillaJsObjects.View = Backbone.View.extend({
-    constructor: function(inspectable) {
+    constructor: function(inspectable, options) {
       this.inspectable = inspectable;
-    };
+      this.options = options;
+      if(typeof inspectable === 'object' && inspectable !== null) {
+        if(isArray(inspectable)) {
+          this.collection = new Backbone.VanillaJsObjects.Array(inspectable);
+        } else {
+          this.collection = new Backbone.VanillaJsObjects.Object(inspectable);
+        }
+      } else {
+        this.model = new Backbone.VanillaJsObjects.Property(inspectable);
+      }
+    }
   });
 })();
--- test/backbone-vanilla-js-objects.js
+++ test/backbone-vanilla-js-objects.js
@@ -1,8 +1,12 @@
 describe("Backbone.VanillaJsObjects", function() {
   var bvo = null;
+  var simples = null;
+  
   beforeEach(function() {
     bvo = Backbone.VanillaJsObjects;
-  })
+    simples = [1, "foo", true, false, null, undefined];
+  });
+
   describe("View", function() {
     var view = null;
 
@@ -13,6 +17,28 @@ describe("Backbone.VanillaJsObjects", function() {
     it("can be given an Object", function() {
       view = new bvo.View(new Object());
       expect(view.collection).toBeDefined();
+      expect(view.collection).toEqual(jasmine.any(bvo.Object));
+    });
+
+    it("can be given an Array", function() {
+      view = new bvo.View(new Array());
+      expect(view.collection).toBeDefined();
+      expect(view.collection).toEqual(jasmine.any(bvo.Array));
+      console.log(view.collection instanceof bvo.Array);
+    });
+
+    describe("can be given simple types", function() {
+      simples.forEach(function(simple) {
+        it("(" + simple + ")", function() {
+          view = new bvo.View(simple);
+          expect(view.model).toBeDefined();
+          expect(view.model).toEqual(jasmine.any(bvo.Property));
+        });
+      });
+    });
+
+    describe("simple types", function() {
+
     });
   });
 });
